8e58c81786b208cc0a1a6551a6e00938e866a819
Akhmad Sadullaev <s.ahmad2702@gmail.com>
2019-06-21 17:03:41 +0200

    other func is activated

--- src/main/java/com/sadullaev/htw/ai/bachelor/app/BackendApp.java
+++ src/main/java/com/sadullaev/htw/ai/bachelor/app/BackendApp.java
@@ -30,7 +30,7 @@ public class BackendApp
         
         System.out.println("Started!");
         
-        /*
+
     	get("/hello", (request, response)->{
     		response.type("text/plain");
     		return "Hallo";
@@ -78,7 +78,7 @@ public class BackendApp
     		
     		return eventManager.getAll(number);
     	});
-        */
+
         
         
         //Free Rooms
